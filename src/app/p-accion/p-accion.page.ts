d25678ec6499ce48a5902ca299af2f2067a942fe
Ariel Dev <ariel.lizana@gmail.com>
2019-05-24 14:20:37 -0400

    Se agrega clasificación usando los checkbox

--- src/app/p-accion/p-accion.page.ts
+++ src/app/p-accion/p-accion.page.ts
@@ -10,6 +10,11 @@ export class PAccionPage implements OnInit {
 
   dragon: boolean;
   mostrarDragon: boolean;
+  uno: any = false;
+  dos: any = false;
+  tres: any = false;
+  cuatro: any = false;
+  cinco: any = false;
 
   constructor(
     public navController: NavController,
@@ -55,6 +60,84 @@ export class PAccionPage implements OnInit {
       ]
     });
     share.present();
+  }
+
+  verEstrellas5() {
+    console.log('Ver estrellas 5');
+    console.log(this.uno);
+    console.log(this.dos);
+    console.log(this.tres);
+    console.log(this.cuatro);
+    console.log(this.cinco);
+    if (this.cinco) {
+      this.cuatro = true;
+      this.tres = true;
+      this.dos = true;
+      this.uno = true;
+    }
+  }
+
+  verEstrellas4() {
+    console.log('Ver estrellas 4');
+    console.log(this.uno);
+    console.log(this.dos);
+    console.log(this.tres);
+    console.log(this.cuatro);
+    console.log(this.cinco);
+    if (this.cuatro) {
+      this.tres = true;
+      this.dos = true;
+      this.uno = true;
+    } else {
+      this.cinco = false;
+    }
+  }
+
+  verEstrellas3() {
+    console.log('Ver estrellas 3');
+    console.log(this.uno);
+    console.log(this.dos);
+    console.log(this.tres);
+    console.log(this.cuatro);
+    console.log(this.cinco);
+    if (this.tres) {
+      this.dos = true;
+      this.uno = true;
+    } else {
+      this.cinco = false;
+      this.cuatro = false;
+    }
+  }
+
+  verEstrellas2() {
+    console.log('Ver estrellas 2');
+    console.log(this.uno);
+    console.log(this.dos);
+    console.log(this.tres);
+    console.log(this.cuatro);
+    console.log(this.cinco);
+    if (this.dos) {
+      this.uno = true;
+    } else {
+      this.cinco = false;
+      this.cuatro = false;
+      this.tres = false;
+    }
+  }
+
+  verEstrellas1() {
+    console.log('Ver estrellas 1');
+    console.log(this.uno);
+    console.log(this.dos);
+    console.log(this.tres);
+    console.log(this.cuatro);
+    console.log(this.cinco);
+    if (!this.uno) {
+      this.cinco = false;
+      this.cuatro = false;
+      this.tres = false;
+      this.dos = false;
+    }
   }
 
 }
